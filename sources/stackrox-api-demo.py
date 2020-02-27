777210586f24975e6e77dc12b8fdd6e0e6879682
Udayabaskar Nachimuthu <nubaskar@gmail.com>
2020-02-26 19:27:30 -0800

    Test

--- sources/stackrox-api-demo.py
+++ sources/stackrox-api-demo.py
@@ -2,31 +2,43 @@ from json import loads
 import requests
 import sys
 
-def check_image():
-    method = "POST"
-    url = "https://container-security.dev.secops.rpaworker.com/image/container/check"
-    data = '{"imageName": "405999462422.dkr.ecr.us-east-1.amazonaws.com/golang:latest","force": true}'
-    headers = {
-            "Content-Type": "application/json",
-            "X-API-KEY": "6WN4BgpRfvdUxPZU7DABAfPCXjcaDwsvy6F7YdegNzb96sjmBV3qhY66nEmswQ5j"
-    }
+# Globals
+url = "https://container-security.dev.secops.rpaworker.com/image/container/check"
+api_key = "6WN4BgpRfvdUxPZU7DABAfPCXjcaDwsvy6F7YdegNzb96sjmBV3qhY66nEmswQ5j"
 
-    return requests.request(
-            method = method,
-            url = url,
-            data = data, 
-            headers = headers,
-            verify = False
-    )
+def check_image(image_name):
+	method = "POST"
+	data = '{"imageName": "' + image_name + '","force": true}'
+
+	response = requests.request(
+		method = method,
+		url = url,
+		data = data, 
+		headers = {
+			"Content-Type": "application/json",
+			"X-API-KEY": api_key
+		},
+		verify = False
+	)
+
+	result = loads(response.content.decode('utf-8'))["result"]
+	print(result)
+
+	return (len(result["alerts"]) == 0
 
 if __name__ == "__main__":
-    response = check_image()
-    result = loads(response.content.decode('utf-8'))["result"]
-    print(result)
+	#---------------------------------------------------------------
+	# Validate with Check API
+	#---------------------------------------------------------------
+	image_name = "405999462422.dkr.ecr.us-east-1.amazonaws.com/golang:latest"
+	
+	if not check_image(image_name):
+		print("Check Failed!")
+		sys.exit(1)
 
-#     if len(result["alerts"]) == 0:
-#         sys.exit(0)
-#     else:
-#         sys.exit(1)
-#     sys.exit(0)
+	#---------------------------------------------------------------
+	# Continue if Check Passed
+	#---------------------------------------------------------------
+	print("Check Passed!")
+	sys.exit(0)
 
