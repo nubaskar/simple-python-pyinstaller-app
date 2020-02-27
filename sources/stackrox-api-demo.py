ad1a9093cc9b4f3ab3cabac0f90d92ed0723c100
Udayabaskar Nachimuthu <nubaskar@gmail.com>
2020-02-26 19:49:26 -0800

    Test

--- sources/stackrox-api-demo.py
+++ sources/stackrox-api-demo.py
@@ -2,29 +2,29 @@ from json import loads
 import requests
 import sys
 
-# # Globals
-# url = "https://container-security.dev.secops.rpaworker.com/image/container/check"
-# api_key = "6WN4BgpRfvdUxPZU7DABAfPCXjcaDwsvy6F7YdegNzb96sjmBV3qhY66nEmswQ5j"
+# Globals
+url = "https://container-security.dev.secops.rpaworker.com/image/container/check"
+api_key = "6WN4BgpRfvdUxPZU7DABAfPCXjcaDwsvy6F7YdegNzb96sjmBV3qhY66nEmswQ5j"
 
-# def check_image(image_name):
-# 	method = "POST"
-# 	data = '{"imageName": "' + image_name + '","force": true}'
+def check_image(image_name):
+	method = "POST"
+	data = '{"imageName": "' + image_name + '","force": true}'
 
-# 	response = requests.request(
-# 		method = method,
-# 		url = url,
-# 		data = data, 
-# 		headers = {
-# 			"Content-Type": "application/json",
-# 			"X-API-KEY": api_key
-# 		},
-# 		verify = False
-# 	)
+	response = requests.request(
+		method = method,
+		url = url,
+		data = data, 
+		headers = {
+			"Content-Type": "application/json",
+			"X-API-KEY": api_key
+		},
+		verify = False
+	)
 
-# 	result = loads(response.content.decode('utf-8'))["result"]
-# 	print(result)
+	result = loads(response.content.decode('utf-8'))["result"]
+	print(result)
 
-# 	return (len(result["alerts"]) == 0
+	return len(result["alerts"]) == 0
 
 if __name__ == "__main__":
 	#---------------------------------------------------------------
